a61f99ab68494b04bd98aba9e1f02c7d75624e3a
Natan Felles <natanfelles@gmail.com>
2016-07-20 04:27:13 -0300

    Atualizada a classe extendida

--- application/models/Installer_model.php
+++ application/models/Installer_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Installer_model extends CI_Controller {
+class Installer_model extends CI_Model {
 
 	/**
 	 * @var array $attributes Atributos obrigatórios das tabelas
